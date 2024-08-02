55cdc750e24ef4d61d0dee20c249234b87bcf3cc
kelsdoerksen <kelseydoerksen@gmail.com>
2024-08-02 11:36:59 -0700

    Fixing embedding feat

--- run_pipeline.py
+++ run_pipeline.py
@@ -67,7 +67,7 @@ def load_connectivity_data(country, buffer_extent, feature_space):
                 .format(root_dir, country, buffer_extent))
 
     if feature_space in ['esa_combined', 'geoclip_combined', 'esa_combined_v04_e008_z18', 'esa_combined_v04_e008_z17',
-                         'esa_combined_z17_v2-embeddings', 'esa_z17_v2-embeddings', 'csp_combined']:
+                         'esa_combined_z17_v2-embeddings', 'csp_combined']:
         eng_train_df = pd.read_csv('{}/{}/{}m_buffer/TrainingData_uncorrelated.csv'.format(root_dir,
                                                                                            country, buffer_extent))
         eng_test_df = pd.read_csv('{}/{}/{}m_buffer/TestingData_uncorrelated.csv'.format(root_dir,
